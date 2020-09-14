ce76215e2b5aa263948f9e36c234dd3dc8f784ed
mbopfNIH <mbopf@mail.nih.gov>
2020-09-14 16:57:43 -0400

    This verion adds option to do the subsampling BEFORE the test/train split, so our test data is much smaller, but conforms to the cohorts.

--- src/multi_predict/predict_main.py
+++ src/multi_predict/predict_main.py
@@ -25,13 +25,20 @@ def parse_args():
     parser.add_argument('--target', required=True, help='Target y variable in dataset')
     parser.add_argument('--infile', required=True, help='Full path of input datafile')
     parser.add_argument('--under_alg', default="RAND", help='"RAND" | <cohort varname>')
-    parser.add_argument('--pred_alg', default="NB", help='Prediction method; NB | LR | SVC | LSVC | MLP | etc')
-    parser.add_argument('--pred_params', default=None, help='String-based Dictionary of algorithm-specific tuning parameters')
+    parser.add_argument('--pred_alg', default="NB",
+                        help='Prediction method; NB | LR | SVC | LSVC | MLP | etc')
+    parser.add_argument('--pred_params', default=None,
+                        help='String-based Dictionary of algorithm-specific tuning parameters')
     parser.add_argument('--seed', default=0, help='Initial random seed')
-    parser.add_argument('--corr_var_file', required=True, help='Path of file containing list of highly-correlated variable names')
-    parser.add_argument('--output_dir', default=None, help='Path to put outputs; if None, no output file produced')
-    parser.add_argument('--samp_strat', default=1.0, help='Value of undersampling "sampling_strategy" param')
+    parser.add_argument('--corr_var_file', required=True,
+                        help='Path of file containing list of highly-correlated variable names')
+    parser.add_argument('--output_dir', default=None,
+                        help='Path to put outputs; if None, no output file produced')
+    parser.add_argument('--samp_strat', default=1.0,
+                        help='Value of undersampling "sampling_strategy" param')
     parser.add_argument('--nproc', default=1, help='Number of proccesses to run')
+    parser.add_argument('--sample_tts', action='store_true',
+                        help='Sample both the test and train data; if not set, only sample training data')
 
     args = parser.parse_args()
     return args
@@ -58,25 +65,30 @@ def main():
     if opts.seed:
         np.random.seed(int(opts.seed))
 
-    # Create stratifed train_test split before under-sampling
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.30)
-    print(f'type(X_train) = {type(X_train)}')
-    print(f'type(y_train) = {type(y_train)}')
-    print(f'X_train.head() = {X_train.head()}')
-    print(f'y_train = {y_train}')
+    # Create stratifed train_test split before under-sampling if opts.sample_tts is set
+    if opts.sample_tts:
+        # No undersampling - NOTE: this could take considerably longer in prediction
+        if opts.under_alg == 'NONE':
+            X_res_t, y_res_t = X, y
+        else:
+            X_res_t, y_res_t = under_samp(X, y, float(opts.samp_strat), opts.target, opts.under_alg)
 
-    # Start timer
-    und_start = time.time()
-    # No undersampling - NOTE: this will take considerably longer in prediction
-    if opts.under_alg == 'NONE':
-        X_res, y_res = X, y
+        print(f'X_res_t.shape = {X_res_t.shape}')
+        print(f'y_res_t.shape = {y_res_t.shape}')
+        X_res, X_test, y_res, y_test = train_test_split(X_res_t, y_res_t, stratify=y_res_t, test_size=0.30)
     else:
-        # Perform undersampling
-        X_res, y_res = under_samp(X_train, y_train, float(opts.samp_strat), opts.target, opts.under_alg)
-        print(f'X_res =\n {X_res}; y_res=\n{y_res}')
-        print(f'np.bincount(y_res)={np.bincount(y_res)}')
+        X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.30)
+        print(f'X_train.shape = {X_train.shape}')
+        print(f'y_train.shape = {y_train.shape}')
+        # No undersampling - NOTE: this could take considerably longer in prediction
+        if opts.under_alg == 'NONE':
+            X_res, y_res = X_train, y_train
+        else:
+            X_res, y_res = under_samp(X_train, y_train, float(opts.samp_strat), opts.target, opts.under_alg)
 
-    und_end = time.time()
+    print(f'X_res =\n {X_res}; y_res=\n{y_res}')
+    print(f'X_res.shape =\n {X_res.shape}; y_res.shape=\n{y_res.shape}')
+    print(f'np.bincount(y_res)={np.bincount(y_res)}')
 
     params_dict = json.loads(opts.pred_params)
     params_list = list(ParameterGrid(params_dict))
@@ -85,15 +97,15 @@ def main():
     pool = mp.Pool(processes=int(opts.nproc))
     for params in params_list:
         if int(opts.nproc) > 1:
-            pool.apply_async(clf_predict, args=(params, X_res, y_res, y_test, opts, X_test))
+            pool.apply_async(clf_predict, args=(params, X_res, y_res, X_test, y_test, opts))
         else:
-            clf_predict(params, X_res, y_res, y_test, opts, X_test)
+            clf_predict(params, X_res, y_res, X_test, y_test, opts)
 
     pool.close()
     pool.join()
 
 
-def clf_predict(params, X_res, y_res, y_test, opts, X_test):
+def clf_predict(params, X_train, y_train, X_test, y_test, opts):
     try:
         clf = None
         if opts.pred_alg == 'NB':
@@ -113,7 +125,7 @@ def clf_predict(params, X_res, y_res, y_test, opts, X_test):
 
         # Classifier
         clf_start = time.time()
-        clf.fit(X_res, y_res)
+        clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
         save_to_file(y_test, y_pred, X_test, clf, clf_start, opts, params)
     except Exception as e:
