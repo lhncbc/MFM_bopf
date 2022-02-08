aad42b61d35c2ad2a62f6802af3a0ccac02a361c
mbopfNIH <mbopf@mail.nih.gov>
2022-02-08 10:40:34 -0500

    These are alternative scripts for running locally on lhc-vm-mbopf

--- src/multi_predict/RF/runClassWgt.sh
+++ src/multi_predict/RF/runClassWgt.sh
@@ -1,3 +1,4 @@
+# Runs on lhc-lx-mbopf
 # Single run for balanced class_weight
 time python ../predict_main.py --target transfus_yes --period='PI' --infile ../../../data/csl/CSL_ty_PI.csv --pred_alg RF --corr_var_file ../../../data/csl/CramerTheil/Cramer_PI_Ty_coeff_Union50.csv --feats='Union50' --under_alg='NONE' --samp_strat 1.0 --sample_tts 0 --feature_thresh 1.0 --pred_params '[{"n_estimators":[128],"criterion":["gini"],"max_depth":[40],"min_samples_leaf":[1],"min_samples_split":[2],"max_leaf_nodes":[100],"max_features":["auto"],"class_weight":["balanced","None"],"random_state":[7]}]' --nproc 1 --seed 3 --output_dir ./output
 
